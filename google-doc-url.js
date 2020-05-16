d4c3a3ca74d23fd65f67272aef498bdeedf1859b
pedro andrade <43356335+opedromandrade@users.noreply.github.com>
2020-05-16 17:14:50 +0100

    .

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GTGyswQoc9iOxe7iXzEdZxSu9lxOkg6_XxIbJWwUq7U/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1u5cCSp_380CBHZTyxQAaKgAzOzmsuYDEH06M5WUrhwM/edit#gid=0';
