2a4670a1452f3c5737cf6dab18a98202beeef2d0
pedro andrade <43356335+opedromandrade@users.noreply.github.com>
2020-05-16 16:58:48 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/13YY8uzI5arZxlLez0GJwK55_BmGmFTXt3opiCGsCKm0/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GTGyswQoc9iOxe7iXzEdZxSu9lxOkg6_XxIbJWwUq7U/edit#gid=0';
